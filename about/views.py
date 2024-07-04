2b6c363f20d3312a7bed77fb6e6c05f924d08cf0
Vilmantas Zaleckas <tiny.post@yahoo.com>
2024-07-04 17:35:25 +0000

    Add post functionallity to views for about form

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -9,6 +10,16 @@ def about_me(request):
     Renders About page
     """
 
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                "Collaboration request received! I endeavour to respond within 2 working days."
+            )
+
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
 
@@ -16,8 +27,8 @@ def about_me(request):
         request,
         "about/about.html",
         {
-            "about":about,
-            "collaborate_form":collaborate_form,
+            "about": about,
+            "collaborate_form": collaborate_form,
             }
     )
 
